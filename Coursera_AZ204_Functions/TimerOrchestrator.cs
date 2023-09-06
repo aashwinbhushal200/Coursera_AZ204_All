53c25c0b7c6f8b2c5ecbe2502b7496fa83a3ace6
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2023-09-06 10:29:31 +0530

    Add project files.

---
+++ Coursera_AZ204_Functions/TimerOrchestrator.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+using System.Timers;
+
+public static class TimerOrchestrator
+{
+    //The following example illustrates how to use durable timers for delay,
+    ///which sends a reminder every day for 10 days.
+    [FunctionName("MyOrchestrator")]
+    public static async Task RunOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext context)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            DateTime deadline = context.CurrentUtcDateTime.AddDays(i);
+            //The createTimer() method is used to create a timer that expires at
+            //the deadline specified by the moment object
+            await context.CreateTimer(deadline, CancellationToken.None);
+            await context.CallActivityAsync("SendReminder", null);
+        }
+    }
+}
---
+++ Coursera_AZ204_Functions/timeout_path.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System.Collections.Generic;
+using System.Threading;
+using Coursera_AZ204_Functions;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+using System.Reflection;
+
+public static class MyOrchestrator
+{
+
+    //durable timers for a timeout, which will execute a different path if a timeout occurs 
+    [FunctionName("MyOrchestrator")]
+    public static async Task<bool> RunOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext context)
+    {
+        DateTime deadline = context.CurrentUtcDateTime.AddSeconds(30);
+
+        Task activityTask = context.CallActivityAsync<bool>("GetQuote", null);
+        Task timeoutTask = context.CreateTimer(deadline, CancellationToken.None);
+
+        List<Task<bool>> tasks = new List<Task<bool>> { activityTask, timeoutTask };
+        while (tasks.Count > 0)
+        {
+            Task<bool> winner = await Task.WhenAny(tasks);
+            if (winner == activityTask)
+            {
+                // success case
+                timeoutTask.Dispose();
+                return true;
+            }
+            else
+            {
+                // timeout case
+                return false;
+            }
+        }
+        return false;
+    }
+}
+
+
+/*js format
+    const df = require("durable-functions");
+const moment = require("moment");
+
+module.exports = df.orchestrator(function * (context) {
+    const deadline = moment.utc(context.df.currentUtcDateTime).add(30, "s");
+    const activityTask = context.df.callActivity("GetQuote");
+    const timeoutTask = context.df.createTimer(deadline.toDate());
+
+    const winner = yield context.df.Task.any([activityTask, timeoutTask]);
+    if (winner === activityTask)
+    {
+        // success case
+        timeoutTask.cancel();
+        return true;
+    }
+    else
+    {
+        // timeout case
+        return false;
+    }
+});*/
