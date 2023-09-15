92f9773baa235a622dec76047790f36331a98b77
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2023-09-16 00:12:09 +0530

    Added unitTest project for az function

--- Coursera_AZ204_Functions/timeout_path.cs
+++ Coursera_AZ204_Functions/timeout_path.cs
@@ -8,38 +8,38 @@ using Coursera_AZ204_Functions;
 using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 using System.Reflection;
 
-public static class MyOrchestrator
-{
-
-    //durable timers for a timeout, which will execute a different path if a timeout occurs 
-    [FunctionName("MyOrchestrator")]
-    public static async Task<bool> RunOrchestrator(
-        [OrchestrationTrigger] IDurableOrchestrationContext context)
+    public static class MyOrchestrator
     {
-        DateTime deadline = context.CurrentUtcDateTime.AddSeconds(30);
 
-        Task activityTask = context.CallActivityAsync<bool>("GetQuote", null);
-        Task timeoutTask = context.CreateTimer(deadline, CancellationToken.None);
-
-        List<Task<bool>> tasks = new List<Task<bool>> { activityTask, timeoutTask };
-        while (tasks.Count > 0)
+        //durable timers for a timeout, which will execute a different path if a timeout occurs 
+        [FunctionName("MyOrchestrator")]
+        public static async Task<bool> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            Task<bool> winner = await Task.WhenAny(tasks);
-            if (winner == activityTask)
+            DateTime deadline = context.CurrentUtcDateTime.AddSeconds(30);
+
+            Task activityTask = context.CallActivityAsync("GetQuote", null);
+            Task timeoutTask = context.CreateTimer(deadline, CancellationToken.None);
+
+            List<Task> tasks = new List<Task> { activityTask, timeoutTask };
+            while (tasks.Count > 0)
             {
-                // success case
-                timeoutTask.Dispose();
-                return true;
+                Task winner = await Task.WhenAny(tasks);
+                if (winner == activityTask)
+                {
+                    // success case
+                    timeoutTask.Dispose();
+                    return true;
+                }
+                else
+                {
+                    // timeout case
+                    return false;
+                }
             }
-            else
-            {
-                // timeout case
-                return false;
-            }
+            return false;
         }
-        return false;
     }
-}
 
 
 /*js format
